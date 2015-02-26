468ff8bc66c8055cc22fdeea4143845f2bc09c24
Mike Gazdag <mike@metalab.co>
2015-02-26 14:34:11 -0800

    Add landing page styles

--- config/environment.rb
+++ config/environment.rb
@@ -11,17 +11,18 @@ require 'slim'
 require 'compass'
 require 'breakpoint'
 require 'susy'
+require 'autoprefixer-rails'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
-# Sinatra's variables
-set :app_file, __FILE__
-set :root, APP_ROOT.to_path
-set :views, File.join(Sinatra::Application.root, "app", "views")
-set :public_folder, 'public'
-
 configure do
+  # Sinatra's variables
+  set :app_file, __FILE__
+  set :root, APP_ROOT.to_path
+  set :views, File.join(Sinatra::Application.root, "app", "views")
+  set :public_folder, 'public'
+
   set :server, :puma
 
   enable :sessions
@@ -37,8 +38,22 @@ configure do
 
   # Compass
   Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = 'views'
+    config.project_path = APP_ROOT.to_path
+    config.sass_dir = 'views/stylessheets'
+    config.css_dir = "/views/stylesheets"
+    config.fonts_dir = "/views/fonts"
+    config.sass_dir = "/views/stylesheets"
+    config.images_dir = "/public/images/"
+
+    config.images_path = "/public/images/"
+    config.javascripts_dir = "/public/javascripts/"
+    config.http_path = "/"
+    config.http_javascripts_dir = "javascripts"
+    config.http_stylesheets_dir = "stylesheets"
+    config.http_images_dir = "images"
+    config.http_fonts_dir = "fonts"
+    config.relative_assets = true
+    config.color_output = true
   end
 
   set :sass, Compass.sass_engine_options
