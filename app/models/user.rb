2998226e8e248cfc961aaf0877b9704bab1234b6
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-01 05:46:07 +0000

    sets email and phone validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :conditions
   has_many :reminders
+
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates :phone, presence: true, format: { with: /\d[10]/i }
+  
 end
