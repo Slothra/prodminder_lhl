f16e42ba2e8a7f9a2d587983305ec151eaede913
Keith <strange.aspiration@gmail.com>
2015-02-28 17:22:26 -0800

    Add new database with env file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227152500) do
+ActiveRecord::Schema.define(version: 20150228153500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,13 +70,13 @@ ActiveRecord::Schema.define(version: 20150227152500) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer  "age"
     t.string   "gender"
     t.string   "email"
     t.string   "phone"
     t.datetime "actived_on"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date_of_birth"
   end
 
 end
