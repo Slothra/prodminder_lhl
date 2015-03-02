7cc5d7f1e651fb06e3911fb64771a89b41b6548b
Mike Gazdag <mike@metalab.co>
2015-03-01 21:40:17 -0800

    Add HTML email to new user verification

--- app/helper_methods.rb
+++ app/helper_methods.rb
@@ -78,11 +78,13 @@ end
 
 
 # Mailgun API helper
-def send_email(action)
+def send_email(action, user, session_id)
   mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
+  @user = user
+  @user_session = session_id
   # Retrieve session user
-  user = find_user
+  # user = find_user
 
   # Establish default email address for our outbound emails:
   prodminder_sender   = "Prodminder Notifications <notify@prodminder.com>"
@@ -102,17 +104,18 @@ def send_email(action)
     prodminder_sender = "Prodminder Notifications <notify@prodminder.com>"
     prodminder_subject = 'Sorry to see you go!'
   end
-  
-  data = Multimap.new
-  data[:from] = prodminder_sender
-  data[:to] = user.email
-  data[:subject] = prodminder_subject
-  data[:text] = "We haven't built the email body case statement yet."
-  data[:html] = "<html>HTML version of the body will go here.</html>"
-  
+
+  message_params ={
+    from:      prodminder_sender,
+    to:        @user.email,
+    subject:   prodminder_subject,
+    text:      'It is really easy to send a message!',
+    html:      erb(:verify_email)
+  }
+
   # Send the actual email:
-  mg_client.send_message "mg.prodminder.com", data
-	
+  mg_client.send_message "mg.prodminder.com", message_params
+
 end
 
 # Twilio API helper
@@ -129,9 +132,9 @@ def send_text(action)
 
   # Send text message
   @twilio_client.account.messages.create({
-    :to   => User.phone, # This needs to be sanitized phone number input, i.e. "6041234567"
-    :from => '+15149002273'
-    :body => "Twilio API test text" # We'll need to concatenate our SMS message into here
+    to: User.phone, # This needs to be sanitized phone number input, i.e. "6041234567"
+    from: '+15149002273',
+    body: "Twilio API test text" # We'll need to concatenate our SMS message into here
   })
 end
 
